19e74a7550e664f645eb688f613b274b47ec12e8
Horacio <sanchez.horacio93@gmail.com>
2020-07-29 10:52:53 -0500

    Comment Shop reductor

--- src/main/scala/training/entrypoint/ShopReductor.scala
+++ src/main/scala/training/entrypoint/ShopReductor.scala
@@ -3,6 +3,10 @@ package training.entrypoint
 import training.modules.shops.Shop
 
 trait ShopReductor {
+
+  /**
+   * Create a shop with parameters obtained of mutation
+   */
   def createShop(
                   businessName: String,
                   name: String,
@@ -16,5 +20,8 @@ trait ShopReductor {
                   position: (Long, Long)
                 ): Shop
 
+  /**
+   * Retrive list of all shop registered
+   */
   def all(): List[Shop]
 }
