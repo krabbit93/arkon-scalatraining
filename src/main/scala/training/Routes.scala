828b071f04f9b538c08414e2a220c2e8d13e2f40
Horacio <sanchez.horacio93@gmail.com>
2020-07-29 10:09:36 -0500

    Move routes to file, externally server config

---
+++ src/main/scala/training/Routes.scala
@@ -0,0 +1,37 @@
+package training
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.model.StatusCodes.BadRequest
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{as, complete, entity, get, getFromResource, path, post, _}
+import akka.http.scaladsl.server.Route
+import io.circe.parser.decode
+import training.Marshallers._
+
+import scala.concurrent.ExecutionContextExecutor
+
+object Routes {
+  def all()(implicit executionContext: ExecutionContextExecutor, system: ActorSystem): Route = {
+    val log = Logging(system.eventStream, "routes-resolver")
+
+    post {
+      path("api") {
+        entity(as[String]) { jsonString => {
+          decode[GraphqlRequest](jsonString) match {
+            case Left(failure) => {
+              log.error("Error in reading: {}", failure)
+              complete(BadRequest -> HttpEntity(ContentTypes.`application/json`, "Error in request body"))
+            }
+            case Right(json) => Processor(json)
+          }
+        }
+        }
+      }
+    } ~ get {
+      path("graphiql") {
+        getFromResource("graphiql.html")
+      }
+    }
+  }
+}
--- src/main/scala/training/Server.scala
+++ src/main/scala/training/Server.scala
@@ -3,13 +3,8 @@ package training
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import io.circe.parser._
-import training.Marshallers._
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
@@ -21,43 +16,31 @@ object Server extends App {
   implicit val system: ActorSystem = ActorSystem("sangria-server")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
   val log = Logging(system.eventStream, "sangria-server")
-  val host = "0.0.0.0"
-  val port = 8080
 
-  val route: Route = post {
-    path("api") {
-      entity(as[String]) { jsonString => {
-        decode[GraphqlRequest](jsonString) match {
-          case Left(failure) => {
-            log.error("Error in reading: {}", failure)
-            complete(BadRequest -> HttpEntity(ContentTypes.`application/json`, "Error in request body"))
-          }
-          case Right(json) => Processor(json)
-        }
-      }
-      }
-    }
-  } ~ get {
-    path("graphiql") {
-      getFromResource("graphiql.html")
-    }
-  }
+  val serverConfig = ConfigFactory.load("server")
+  val host = serverConfig.getString("host")
+  val port = serverConfig.getInt("port")
 
+  val route = Routes.all()
   val bindingFuture = Http().bindAndHandle(route, host, port)
 
   bindingFuture.onComplete {
     case Success(serverBinding) =>
       log.info(s"Server bound to ${serverBinding.localAddress}")
+      waitKeyForTermination()
 
     case Failure(ex) =>
       log.error(ex, "Failed to bind to {}:{}!", host, port)
       system.terminate()
   }
 
-  log.info("Press enter key to stop...")
-  StdIn.readLine()
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+  private def waitKeyForTermination() = {
+    log.info("Press enter key to stop...")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
 }
