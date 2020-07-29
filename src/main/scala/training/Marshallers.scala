242fe9bba00df1cea336706e8021fe9c2472a55f
Horacio <sanchez.horacio93@gmail.com>
2020-07-28 19:34:52 -0500

    Marshallers for model classes

---
+++ src/main/scala/training/Marshallers.scala
@@ -0,0 +1,34 @@
+package training
+
+import io.circe.Decoder.Result
+import io.circe.{Decoder, Encoder, HCursor, Json}
+
+object Marshallers {
+  implicit object GraphMarshall extends Decoder[GraphqlRequest] {
+    override def apply(c: HCursor): Result[GraphqlRequest] = {
+      val query = c.downField("query").as[String] match {
+        case Left(_) => throw new IllegalArgumentException
+        case Right(value) => value
+      }
+
+      val operationName = c.downField("operationName").as[String] match {
+        case Left(_) => None
+        case Right(value) => Option(value)
+      }
+
+      val variables = c.downField("variables").as[Map[String, String]] match {
+        case Left(_) => None
+        case Right(value) => Option(value)
+      }
+
+      Right(GraphqlRequest(query, operationName, variables))
+    }
+  }
+
+  implicit object ShopEncoder extends Encoder[CommercialActivity] {
+    override def apply(a: CommercialActivity): Json = Json.obj(
+        ("id", Json.fromInt(a.id)),
+        ("name", Json.fromString(a.name))
+    )
+  }
+}
