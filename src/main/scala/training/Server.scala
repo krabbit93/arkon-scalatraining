3bcbceb53e496be8a3d42942e19230651d181ae0
Horacio <sanchez.horacio93@gmail.com>
2020-07-28 19:35:20 -0500

    Client and simple (graphiql.html) server of graphql

--- src/main/scala/training/Server.scala
+++ src/main/scala/training/Server.scala
@@ -3,14 +3,19 @@ package training
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import io.circe.parser._
+import training.Marshallers._
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 import scala.util.{Failure, Success}
+
+case class GraphqlRequest(query: String, operationName: Option[String], variables: Option[Map[String, String]])
 
 object Server extends App {
   implicit val system: ActorSystem = ActorSystem("sangria-server")
@@ -20,14 +25,23 @@ object Server extends App {
   val host = "0.0.0.0"
   val port = 8080
 
-  val route: Route = path("") {
-    complete(
-      HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>")
-    )
-  } ~ (post & path("api")) {
-    complete(
-      HttpEntity(ContentTypes.`text/html(UTF-8)`, "api graph")
-    )
+  val route: Route = post {
+    path("api") {
+      entity(as[String]) { jsonString => {
+        decode[GraphqlRequest](jsonString) match {
+          case Left(failure) => {
+            log.error("Error in reading: {}", failure)
+            complete(BadRequest -> HttpEntity(ContentTypes.`application/json`, "Error in request body"))
+          }
+          case Right(json) => Processor(json)
+        }
+      }
+      }
+    }
+  } ~ get {
+    path("graphiql") {
+      getFromResource("graphiql.html")
+    }
   }
 
   val bindingFuture = Http().bindAndHandle(route, host, port)
