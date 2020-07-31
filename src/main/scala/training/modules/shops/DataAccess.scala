fedd7b49a940efe3717124d533a2586ba5137f7d
Horacio <sanchez.horacio93@gmail.com>
2020-07-31 17:23:28 -0500

    Externally config using conf file with env variables

--- src/main/scala/training/modules/shops/DataAccess.scala
+++ src/main/scala/training/modules/shops/DataAccess.scala
@@ -2,15 +2,19 @@ package training.modules.shops
 
 import akka.dispatch.ExecutionContexts
 import cats.effect.{ContextShift, IO}
+import com.typesafe.config.{Config, ConfigFactory}
 import doobie.util.transactor.Transactor
+import doobie.util.transactor.Transactor.Aux
 
 object DataAccess {
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContexts.global())
 
-  val xa = Transactor.fromDriverManager[IO](
+  val databaseConf: Config = ConfigFactory.load("database")
+
+  val xa: Aux[IO, Unit] = Transactor.fromDriverManager[IO](
     "org.postgresql.Driver",
-    "jdbc:postgresql://localhost:5432/shops",
-    "admin",
-    "admin123"
+    s"jdbc:postgresql://${databaseConf.getString("host")}:${databaseConf.getInt("port")}/${databaseConf.getString("dbname")}",
+    databaseConf.getString("username"),
+    databaseConf.getString("password")
   )
 }
