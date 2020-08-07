ca54ac445ee3594c47242812af686be9b992f119
Horacio <sanchez.horacio93@gmail.com>
2020-08-07 12:16:08 -0500

    Export dependencies in server

--- src/api/main/scala/training/Server.scala
+++ src/api/main/scala/training/Server.scala
@@ -1,22 +1,16 @@
 package training
 
-import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import io.circe.Json
+import training.config.dependencies._
 
-import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 case class GraphqlRequest(query: String, operationName: Option[String], variables: Json)
 
 object Server extends App {
-  implicit val system: ActorSystem                        = ActorSystem("sangria-server")
-  implicit val materializer: ActorMaterializer            = ActorMaterializer()
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-
   val log = Logging(system.eventStream, "sangria-server")
 
   val serverConfig = ConfigFactory.load("server")
